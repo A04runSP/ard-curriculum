e15565b67051a3bd28ed0039bf8b3edb9d9488a1
Arun <146452650+A04runSP@users.noreply.github.com>
2026-09-16 13:51:39 +0530

    Add Stage 5 expansion metadata mapping

---
+++ src/data/stage5Expansion.meta.js
@@ -0,0 +1,15 @@
+export const stage5ExpansionMeta = {
+  stageId: "web-development",
+  addedLessons: ["l-forms-validation", "l-browser-storage-apis"],
+  addedConcepts: ["c-accessibility-testing-wcag", "c-web-performance"],
+  resourceLinks: [
+    "https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Forms/Your_first_form",
+    "https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Forms/Form_validation",
+    "https://developer.mozilla.org/en-US/docs/Web/API/FormData",
+    "https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API",
+    "https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams",
+    "https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Client-side_APIs/Client-side_storage",
+    "https://www.w3.org/TR/WCAG22/",
+    "https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Performance/What_is_web_performance"
+  ]
+};
