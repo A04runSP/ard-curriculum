9076b08c03011e432ea09f32cf7617fda1982b5a
Arun <146452650+A04runSP@users.noreply.github.com>
2026-09-18 20:25:20 +0530

    refactor: remove nested stage14 import

--- src/data/stage13Expansion.js
+++ src/data/stage13Expansion.js
@@ -2,7 +2,6 @@ import { lessonB4 } from "./lessonB4.js";
 import { lessonB6 } from "./lessonB6.js";
 import { concept3 } from "./concept3.js";
 import "./stage4Expansion.js";
-import "./stage14Expansion.js";
 
 lessonB4.push(...lessonB6);
 
