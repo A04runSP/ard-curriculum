52da2aba4980849747cabdf271ebc0c0d011989c
Arun <146452650+A04runSP@users.noreply.github.com>
2026-09-18 20:25:16 +0530

    refactor: expose stage14 expansion dependency

--- src/data/curriculumExpansions.js
+++ src/data/curriculumExpansions.js
@@ -6,4 +6,5 @@ import "./stage10Expansion.js";
 import "./stage11Expansion.js";
 import "./stage12Gaps.js";
 import "./stage12Expansion.js";
+import "./stage14Expansion.js";
 import "./stage13Expansion.js";
