d2065e7851eb56dda8958e03d6acf546c9a2ea8d
Arun <146452650+A04runSP@users.noreply.github.com>
2026-09-18 19:58:18 +0530

    refactor: expose stage12 gap dependency

--- src/data/curriculumExpansions.js
+++ src/data/curriculumExpansions.js
@@ -4,5 +4,6 @@
 // chain while making the dependency entry point visible in one place.
 import "./stage10Expansion.js";
 import "./stage11Expansion.js";
+import "./stage12Gaps.js";
 import "./stage12Expansion.js";
 import "./stage13Expansion.js";
